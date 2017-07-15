b0178e9448423ae5b01a9f62c4b75741c74140c0
Alex <104133.palmer@gmail.com>
2017-07-15 16:57:29 +1000

    Fixed shutdown and info

--- utilities.py
+++ utilities.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from datetime import timedelta
 log = logging.getLogger()
 startTime = time.localtime()
-ownerID = '66141201631285248'
+ownerID = 66141201631285248
 class Utilities:
     """ Commands relating to the Blitzcrank Bot's operations."""
     def __init__(self, bot):
@@ -31,19 +31,18 @@ class Utilities:
 
     @commands.command(no_pm=True)
     async def shutdown(self, ctx):
-        if ctx.author.id == ownerID:
+        if ctx.message.author.id == ownerID:
             await self.bot.logout()
         else:
             await ctx.send("Sorry, you don't have permission to use that command!")
 
     @commands.command(pass_context=True, no_pm=True)
     async def info(self, ctx):
-        info = ("A simple bot in it's first iteration for League of Legends "
+        info = ("A simple bot in it's second iteration for League of Legends "
                 "summoner look ups. Written using discord.py by "
                 "Frosty ☃#5263.")
-        await ctx.send(ctx.message.channel, info)
+        await ctx.send(info)
 def setup(bot):
     bot.add_cog(Utilities(bot))
 
-#TODO: fix ping command not editing.
-#Fix on_command errors
+#Base finished
