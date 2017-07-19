cbb70798974171b7665cc091221ab8d12cd8a4a0
Alex <104133.palmer@gmail.com>
2017-07-19 17:07:29 +1000

    Custom help command

--- static_data.py
+++ static_data.py
@@ -1,4 +1,5 @@
 import asyncio
+from discord import Embed
 from discord.ext import commands
 
 
@@ -7,6 +8,30 @@ class Help:
 
     def __init__(self, bot):
         self.bot = bot
+        bot.remove_command("help")
+
+    @commands.group()
+    async def help(self, ctx):
+        if ctx.invoked_subcommand is None:
+            embed=Embed(colour=0x00ff00)
+            embed.set_author(name="Blitzcrank Bot - Commands:", icon_url=self.bot.user.avatar_url)
+            embed.add_field(name="b!search 'User'", value="Show a user's ranked statistics", inline=True)
+            embed.add_field(name="Example:", value="b!search Riviere", inline=True)
+            embed.add_field(name="b!mastery 'User' 'Champ'", value="Shows a user's champ mastery", inline=True)
+            embed.add_field(name="Example:", value="b!mastery Riviere Sivir", inline=True)
+            embed.add_field(name="b!region view", value="Show the default region", inline=False)
+            embed.add_field(name="b!region set 'region'", value="Set the server's default region", inline=True)
+            embed.add_field(name="Example:", value="b!region set OCE", inline=True)
+            embed.add_field(name="b!region update 'region'", value="Update server's default region", inline=True)
+            embed.add_field(name="Example:", value="b!region update NA", inline=True)
+            embed.add_field(name="b!region remove", value="Removes server's default region", inline=True)
+            embed.add_field(name="b!regions", value="Show a list of all valid regions", inline=False)
+            embed.add_field(name="Other commands:", value="Other commands can be listed with b!help more", inline=True)
+            await ctx.send("", embed=embed)
+    
+    @help.command()
+    async def more(self, ctx):
+        await ctx.send("Coming later! Don't worry, all the important commands are already listed!")
 
     @commands.command(no_pm=True)
     async def regions(self, ctx):
