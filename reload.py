d1ea47522dcbeede932d93cc8329883f8354f078
Alex Palmer <apal0934@uni.sydney.edu.au>
2017-07-20 01:51:47 +1000

    Moved owner check from manual to extension

--- reload.py
+++ reload.py
@@ -10,19 +10,17 @@ class Reload:
         self.bot = bot
 
     @commands.command(no_pm=True, hidden=True, description="Test")
+    @commands.is_owner()
     async def reload(self, ctx, *, module: str):
         """Reloads the specified module."""
-        if ctx.message.author.id == config.OWNER_ID:
-            try:
-                self.bot.unload_extension(module)
-                self.bot.load_extension(module)
-            except Exception as e:
-                await ctx.send("There was an error reloading module {}:".format(module))
-                await ctx.send("```fix\n{}: {}\n```".format(type(e).__name__, e))
-            else:
-                await ctx.send("{} successfully reloaded.".format(module))
+        try:
+            self.bot.unload_extension(module)
+            self.bot.load_extension(module)
+        except Exception as e:
+            await ctx.send("There was an error reloading module {}:".format(module))
+            await ctx.send("```fix\n{}: {}\n```".format(type(e).__name__, e))
         else:
-            await ctx.send("Sorry, you don't have permission to use that command!")
+            await ctx.send("{} successfully reloaded.".format(module))
 
 
 def setup(bot):
